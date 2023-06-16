784737f76c5e4fcb5409dc9c52024c82e6eb7d4b
Joshua Owusu <joshbillion1@gmail.com>
2023-06-16 10:16:56 +0000

    Commit auto_update

--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -8,14 +8,14 @@ console.log(greetings);
 let userId = 334455.3
 
 userId.toFixed()
-// userId = "joshua"
+// userId = "joshua" this will show an error since its supposed to be a number.
+
 // boolean
 let isLoggedIn: boolean = false
 
 
 
 // any
-
 let hero: string;
 
 function getHero(){
