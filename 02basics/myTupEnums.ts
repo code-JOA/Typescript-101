f8a1eaa6ca96e53347aaaaaa93129c0ae8ea47c3
Joshua Owusu <joshbillion1@gmail.com>
2023-06-16 10:42:55 +0000

    Commit auto_update

--- 02basics/myTupEnums.ts
+++ 02basics/myTupEnums.ts
@@ -1,7 +1,7 @@
 // const user: (string | number)[] = [1, "hc"]
 let tUser: [string, number, boolean]
 
-tUser = ["jj", 131, true]
+tUser = ["hc", 131, true]
 
 let rgb: [number, number, number] = [255, 123, 112]
 
@@ -9,8 +9,11 @@ type User = [number, string]
 
 const newUser: User = [112, "example@google.com"]
 
-newUser[1] = "jj.com"
+newUser[1] = "hc.com"
 newUser.push(true)
+
+
+export {}
 
 
 
