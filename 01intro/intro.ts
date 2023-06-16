115e0bf73ce8c7632c2750a79176b90f8a4a9238
Joshua Owusu <joshbillion1@gmail.com>
2023-06-16 10:11:28 +0000

    Commit auto_update

--- 01intro/intro.ts
+++ 01intro/intro.ts
@@ -1,4 +1,9 @@
-let myuser = { name: "joshua", age: 25 }
+// let myuser = {name: "giulia", age: 10}
+
+// console.log("giulia");
+//console.log(user.email);
+
+let newUser = { name: "joshua", age: 25 }
 
 console.log("joshua");
 
