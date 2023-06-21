a8adf296d86dc03e4cc499aed246c8959859dc12
Joshua Owusu <joshbillion1@gmail.com>
2023-06-21 11:46:53 +0000

    Commit auto_update

--- 1hour-tut/react/src/components/postCard/PostCard.tsx
+++ 1hour-tut/react/src/components/postCard/PostCard.tsx
@@ -13,8 +13,9 @@
 // export default PostCard;
 
 import React from "react";
+import { PostProps } from "@/types/types";
 
-const PostCard = (props: { title: string; body: string }) => {
+const PostCard = (props: PostProps) => {
   return (
     <div className="postCard">
       <h1>{props.title}</h1>
