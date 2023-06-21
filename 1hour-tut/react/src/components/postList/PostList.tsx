5d2e041385de32b141b703233a7317f715adc302
Joshua Owusu <joshbillion1@gmail.com>
2023-06-21 10:14:43 +0000

    Commit auto_update

--- 1hour-tut/react/src/components/postList/PostList.tsx
+++ 1hour-tut/react/src/components/postList/PostList.tsx
@@ -1,28 +1,34 @@
+// import React from "react";
+// import PostCard from "../postCard/PostCard";
+// import { PostProps } from "@/types/types";
+
+// async function getData() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+
+//   return res.json();
+// }
+
+// const PostList = async () => {
+//   const data: PostProps[] = await getData();
+//   return (
+//     <div className="postList">
+//       {/* <PostCard title="post title" body="post desc" /> */}
+
+//       {data.map((post) => (
+//         <PostCard key={post.id} {...post} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default PostList;
+
 import React from "react";
-import PostCard from "../postCard/PostCard";
-import { PostProps } from "@/types/types";
 
-async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
+export default function PostList() {
+  return <div>PostList</div>;
 }
-
-const PostList = async () => {
-  const data: PostProps[] = await getData();
-  return (
-    <div className="postList">
-      {/* <PostCard title="post title" body="post desc" /> */}
-
-      {data.map((post) => (
-        <PostCard key={post.id} {...post} />
-      ))}
-    </div>
-  );
-};
-
-export default PostList;
