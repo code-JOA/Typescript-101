c29053e4eb6d3c0d9f0171719bc015bfcbcc7cda
Joshua Owusu <joshbillion1@gmail.com>
2023-06-21 11:51:27 +0000

    Commit auto_update

--- 1hour-tut/react/src/components/postCard/PostCard.tsx
+++ 1hour-tut/react/src/components/postCard/PostCard.tsx
@@ -1,27 +1,27 @@
-// import { PostProps } from "@/types/types";
-// import React from "react";
-
-// const PostCard = ({title,body}: PostProps) => {
-//   return (
-//     <div className="postCard">
-//       <h1>{title}</h1>
-//       <p>{body}</p>
-//     </div>
-//   );
-// };
-
-// export default PostCard;
-
-import React from "react";
 import { PostProps } from "@/types/types";
+import React from "react";
 
-const PostCard = ({title,body}:{ props }: PostProps) => {
+const PostCard = ({ title, body }: PostProps) => {
   return (
     <div className="postCard">
-      <h1>{props.title}</h1>
-      <p>{props.body}</p>
+      <h1>{title}</h1>
+      <p>{body}</p>
     </div>
   );
 };
 
 export default PostCard;
+
+// import React from "react";
+// import { PostProps } from "@/types/types";
+
+// const PostCard = ({title,body}:{ props }: PostProps) => {
+//   return (
+//     <div className="postCard">
+//       <h1>{props.title}</h1>
+//       <p>{props.body}</p>
+//     </div>
+//   );
+// };
+
+// export default PostCard;
